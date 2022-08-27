bcc6877b90ceb696e315b85354787c6c1e4749a2
Alexandre Paiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 10:12:39 -0300

    Mudança de pasta - Delete PYTHON

--- PYTHON/Básico/print.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-# #Argumento nomeado
-#Argumentos nomeados muitos utilizados são sep (que adiciona separação) e end (que adiciona coisas ao final do código)
-
-print('Luiz', 'Otávio', sep='-', end='#')
