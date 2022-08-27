00851f23b4af8d3c88f697117b96c944aa1d01b4
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 10:32:18 -0300

    Variáveis

---
+++ python/Básico/variáveis.py
@@ -0,0 +1,15 @@
+"""" 
+
+Variáveis devem iniciar com letra, pode conter números, separar _, letras minusculas
+
+"""
+
+nome = 'Alexandre Paiva'; #str
+idade = 23; #int
+altura = 1.85; #float
+maiorIdade = idade >= 18; #bool
+
+print ('Nome: ', nome);
+print ('Idade: ', idade);
+print ('Altura: ', altura);
+print ('É maior de idade? ', maiorIdade);
