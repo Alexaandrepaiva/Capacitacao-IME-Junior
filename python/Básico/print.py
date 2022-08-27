77b470627771db4c594353f48d7e0025844c399b
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 10:14:54 -0300

    Aula inicial

---
+++ python/Básico/print.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# #Argumento nomeado
+#Argumentos nomeados muitos utilizados são sep (que adiciona separação) e end (que adiciona coisas ao final do código)
+
+print('Luiz', 'Otávio', sep='-', end='#')
---
+++ python/Básico/string.py
@@ -0,0 +1,9 @@
+'''
+Strings são textos entre aspas
+Python é uma linguagem de tipagem dinâmica
+O caractere de escape é uma contra-barra \
+O comando para quebrar linha é \n
+Ao adicionar "r" antes do str, o python não vai executar comandos que estiverem dentro do str
+'''
+
+print(r'Esse é meu \n string teste')
