d4b9b6b752cdda5ed19c1abb140feba7ece12359
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 16:10:18 -0300

    Condições

---
+++ python/Básico/condições.py
@@ -0,0 +1,20 @@
+#Condições: if, elif, else
+""" 
+Operadores Relacionais:
+== igualdade
+> maior que
+>= maior que ou igual a
+< menor
+<= menor que ou igual a
+!= diferente
+
+Eles sempre retonar booleanos: True/False
+
+"""
+
+if False:
+    print('A primeira coisa foi verdadeira.');
+elif True:
+    print('A segunda coisa é verdadeira');
+else:
+    print('A terceira coisa é verdadeira');
