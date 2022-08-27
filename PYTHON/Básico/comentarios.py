67e093ee925de0a0a1734fc085ab72f7b1898de6
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 09:16:24 -0300

    Início do curso básico de python

---
+++ PYTHON/Básico/comentarios.py
@@ -0,0 +1,19 @@
+# Isso é um comentário de uma linha
+
+""" 
+Para comentários de muitas linhas
+Essa é a maneira correta
+"""
+
+print('Linha 1')
+print('Linha 2')
+print('Linha 3')
+#Falando sobre o que se trata essa linha
+print('Linha 4')
+print('Linha 5')
+print('Linha 6')
+
+'''
+Mas, outro tipo de comentário multilinhas
+É o cometnário com aspas simples
+'''
---
+++ PYTHON/Básico/primitivos.py
@@ -0,0 +1,15 @@
+""" 
+Tipos de Dados Primitivos
+
+str - string #textos 'Assim' ou "assim"
+int - inteiro #não tem ponto
+float - ponto flutuante #tem ponto 
+bool - booleano #True/False
+
+"""
+print('Alexandre', type('Alexandre'));
+print('10', type('10'));
+print(10, type(10));
+print(25.23, type(25.23));
+print(10 == 10, type(10 == 10));
+print('l' == 'L', type('l' == 'L'));
