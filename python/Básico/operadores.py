603f1733971e713ce03e0ae862b1dcb432bc11aa
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 10:10:17 -0300

    Operadores Aritméticos

---
+++ python/Básico/operadores.py
@@ -0,0 +1,24 @@
+""" 
+
++ ADIÇÃO, - SUBTRAÇÃO, * MULTIPLICAÇÃO, / DIVIÇÃO
+// VALOR INTEIRO, ** POTENCIAÇÃO, % RETORNA O RESTO DA DIVISÃO, () ALTERA A PRECEDÊNCIA
+
+"""
+
+#A multiplicação também opera em um inteira e uma string:
+print(10 * 'Alexandre ')
+
+#A adição pode concatenar duas strings:
+print('Alexandre' + ' ' + 'Paiva')
+
+#A precedência dos operadores é a mesma da lógica matemática:
+
+""" 
+
+A precedência, da maior para a menor, é:
+1. ()
+2. **
+3. *, /, //, %
+4. +, -
+
+"""
