c6ea707bd0c22802d3186183d643e24af08a461a
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 09:30:35 -0300

    Tipos de dados primitivos

--- PYTHON/Básico/primitivos.py
+++ PYTHON/Básico/primitivos.py
@@ -12,4 +12,22 @@ print('10', type('10'));
 print(10, type(10));
 print(25.23, type(25.23));
 print(10 == 10, type(10 == 10));
-print('l' == 'L', type('l' == 'L'));
+print('l' == 'L', type('l' == 'L'), '\n'); 
+
+#A maioria das coisas que tiver vazias, tem valor booleano de falso
+print(bool(''))
+print(bool(0))
+print(bool([]), '\n')
+
+#É possível converter o tipo de Dados:
+print('10', type('10'), type(int('10')), '\n')
+
+#Mas isso pode dar erro, caso a gente tente conver uma string em int ou um float em int, como por exemplo:
+#print('alexandre', int('alexandre'));
+#print('20.25', int('20.25'));
+
+#Exercíco Cadastro 
+print('alexandre', type('alexandre')) #nome
+print(23, type(23)) #idade
+print(1.85, type(1.85)) #altura
+print(23 >= 18, type(23 >= 18)) #maior de idade
