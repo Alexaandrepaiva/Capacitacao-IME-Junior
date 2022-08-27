8af1eaca231e2b4ae0209991a2fbbb9ecf23547d
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-27 11:39:55 -0300

    Input

---
+++ python/Básico/input.py
@@ -0,0 +1,7 @@
+#entrada de dados
+#sempre vai retornar uma string
+
+nome = input("Qual é o seu nome? ");
+idade = input('Qual é a sua idade? ');
+
+print(f'\nO(a) {nome} tem {idade} anos de idade.')
