e9e6b5d93303846693bae93e4e3ceac8d1048d69
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-09-05 13:33:29 -0300

    Numbers

---
+++ nodejs/JavaScript/Básico/number.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1.toString()+num2.toString())
+
+//Representação na base n = .toString(n)
+
+console.log(num1.toString(2))
+
+//Arredondar para n casas decimais = .toFixed(n)
