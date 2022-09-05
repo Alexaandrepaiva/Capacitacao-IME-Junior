1eee47c778c5340fb635fe012fce70b5f12212c1
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-09-05 12:57:14 -0300

    Complementação strings

--- nodejs/JavaScript/Básico/consolelog.js
+++ nodejs/JavaScript/Básico/consolelog.js
@@ -6,7 +6,16 @@ console.log("Essa é a minha primeira frase de teste") // string
 console.log(123123, 15.85, 35, 29.90) // number
 
 //Concatenação de Strings
-let umaString = "Eu não gosto de "
-console.log(umaString.concat('cereja e banana'));
-console.log(umaString + 'abacaxi e banana');
-console.log(`${umaString}banana e melancia`);
+let umaString = "Eu não gosto de frutas, MAS gosto de"
+console.log(umaString.concat(' cereja e banana'));
+console.log(umaString + ' abacaxi e banana');
+console.log(`${umaString} banana e melancia`);
+
+
+//Para repartir uma string
+console.log(umaString.split(' ', 3))
+console.log(umaString.split(' '))
+
+// Para alterar a formatação da string
+console.log(umaString.toUpperCase())
+console.log(umaString.toLowerCase())
