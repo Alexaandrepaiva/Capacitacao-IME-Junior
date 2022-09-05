57a3ea27cd2d19cfdd4a32ba67a8947316207b07
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-09-05 09:22:31 -0300

    Dados primitivos

--- nodejs/JavaScript/Básico/dadosPrimitivos.js
+++ nodejs/JavaScript/Básico/dadosPrimitivos.js
@@ -1,1 +1,8 @@
-// String, number, undefined
+// String, number, undefined, null, boolean, symbol
+const nome = 'Alexander'; // stirng
+let numero = 123123; // number
+let nomeAluno; // undefined
+let corDeFundo = null // nulo
+let boleano = true // boolean
+
+//undefined e nulo não apontam para local nenhum da memória
