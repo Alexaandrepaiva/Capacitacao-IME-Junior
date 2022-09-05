bf65b5ae3743947ccbb63a95daf4b01ef151d21c
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-09-05 08:47:20 -0300

    Variáveis

---
+++ nodejs/JavaScript/Básico/consolelog.js
@@ -0,0 +1,6 @@
+// Para exibir algo na tela, usamos console.log()
+// Dentro dos parêntes, coloa-se o argumento da função. Para colocar uma string, deve-se envolver o texto por aspas (simples ou dupla) ou crase
+
+console.log("Essa é a minha primeira frase de teste") // string
+
+console.log(123123, 15.85, 35, 29.90) // number
---
+++ nodejs/JavaScript/Básico/variaveis.js
@@ -0,0 +1,14 @@
+let nome = 'João'; // String
+
+// Evitar usar var, mas existe em códigos antigos
+var numero = 123123
+
+// Uma variavel pode ser declarada ser ser inicializada
+let variavel; // Tem valor de undefined
+variavel = 24.25 // Inicialização da variável
+console.log(variavel)
+variavel = 29.14 // Re-nicialização da variável
+console.log(variavel)
+    
+// Uma variável não pode ser redeclarada com let, apenas com var
+// Não se pode usar uma variável com palavra reservada (if, var, let, ...)
