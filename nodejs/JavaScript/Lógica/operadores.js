625ae5e56e34a2ed5f93d7dbf37302711f172d8c
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-09-07 08:47:23 -0300

    Operadores

---
+++ nodejs/JavaScript/Lógica/operadores.js
@@ -0,0 +1,22 @@
+/* 
+
+Os operadores de comparação são:
+> maior do que
+>= maior do que ou igual a
+< menor do que
+<= mnor do que ou igual a 
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrita (valor e tipo)
+
+*/
+
+/*
+
+Os operadores de lógica são:
+&& - and - e
+|| - or - ou
+! - not - não
+
+*/
