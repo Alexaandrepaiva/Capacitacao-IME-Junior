3d4482e4e67680e691e9a4d497f6eb81f13cf0d8
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-09-07 09:56:25 -0300

    Create curtoCircuito.js

---
+++ nodejs/JavaScript/Lógica/curtoCircuito.js
@@ -0,0 +1,13 @@
+/*
+
+&& - retorna o último valor, "valor mesmo" verdadeuro ou o primeiro falso encontrado
+|| - retorna o primeiro valor verdadeiro, "valor mesmo", ou o último falso
+
+Os valores que js interpreta como falso (FALSY) são: 
+false
+0
+NaN
+'', "", ``
+null, undefined
+
+*/
