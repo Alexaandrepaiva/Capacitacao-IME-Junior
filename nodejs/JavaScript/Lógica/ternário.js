5ff13da4fb36d605b1c67fa793c5633f83169217
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-10-10 07:17:58 -0300

    Reestruturação

--- nodejs/JavaScript/Lógica/ternário.js
+++
@@ -1,9 +0,0 @@
-// condição ? valor para verdadeiro : valor para falso
-
-const pontuacaoUsuario = 1500;
-const nivelUsuario = pontuacaoUsuario >=1000? 'Usuario VIP':'Usuario normal';
-
-const corPadrao = 'red';
-const corDeFundo = corPadrao || 'black';
-
-console.log(nivelUsuario, corDeFundo);
