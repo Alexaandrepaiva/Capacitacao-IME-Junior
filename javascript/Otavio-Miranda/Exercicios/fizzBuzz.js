a0f764602e719bb2819e52544624dda362fa1bc3
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2023-02-11 08:57:44 -0300

    js

---
+++ javascript/Otavio-Miranda/Exercicios/fizzBuzz.js
@@ -0,0 +1,11 @@
+// Escreva uma função que recebe um número e retorne o seguinte:
+// número é divisível por 3: Fizz
+// número é divisível por 5: Buzz
+// número é divisível por 3 e por 5: FizzBuzz
+// número não é divisível por 3 e nem por 5: retorna o próprio número
+// checar se o número é realmente um número
+// use a função com um número inteiro randomizado de 0 a 100.
+
+function Fizzbuzz (number) {
+    
+}
