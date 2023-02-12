adc9c34f6465e8a0364011aa72b30929ddfd408d
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2023-02-12 13:14:13 -0300

    Create cmath.cpp

---
+++ cpp/BroCode/cmath.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <cmath>
+
+int main(){
+
+    double x = 3;
+    double y = 4;
+    double a, b, c, d;
+
+    a = std::max(x,y);
+    std:: cout << a <<"\n";
+
+    b = std::min(x,y);
+    std:: cout << b <<"\n";
+    
+    c = pow(2, 4);
+    std:: cout << c <<"\n";
+
+    d = sqrt(2);
+    std:: cout << d <<"\n";
+
+
+    return 0;
+}
